d4ca8749feabee44a0e73ac0446ddd4d77f4ea3c
williamyhg1 <90809764+williamyhg1@users.noreply.github.com>
2022-10-17 23:37:10 +1100

    first commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("./form", formHandler)
+	http.HandleFunc("./hello", helloHandler)
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
